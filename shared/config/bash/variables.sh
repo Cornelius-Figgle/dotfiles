f7ea2e7d69c34a67fa7f8142e696e0540757fcf7
Max <max@fullimage.net>
2023-07-14 17:06:54 +0100

    Create variables.sh

---
+++ shared/config/bash/variables.sh
@@ -0,0 +1,19 @@
+# "In summary, we primarily use the set command to configure error handling,
+# debugging, and script behavior within the local scope of a script or shell
+# session. On the other hand, we use the export command to create global environment
+# variables that other processes or scripts running in the same session can access"
+# 
+# for more info: https://www.baeldung.com/linux/bash-set-and-export
+
+export PATH="$PATH:/home/max/.local/bin:/home/max/.cargo/bin"
+export XDG_SCREENSHOTS_DIR="/tmp/Screenshots"
+export EDITOR="helix"
+
+PS1='\w \$ '
+
+export PYTHONIOENCODING="utf-8"
+eval "$(thefuck --alias)"
+
+export ProgressPreference='SilentlyContinue'
+
+export WSL_UTF8=1
