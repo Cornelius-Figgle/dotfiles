c18923c559cdb7dbcdbf0439c7b09f5a8b5f4674
Cornelius-Figgle <max@fullimage.net>
2023-08-04 19:06:06 +0100

    f

--- scripts/wallpaper_delta.sh
+++ scripts/wallpaper_delta.sh
@@ -6,9 +6,11 @@ echo "splash = false" > ~/.config/hypr/hyprpaper.conf
 echo "ipc = off" >> ~/.config/hypr/hyprpaper.conf
 monitors=$(hyprctl monitors -j | jq -r ".[] | .name")
 
+
+	wallpaper=$(fd ".png|.jpg|.jpeg" ~/source/dotfiles/wallpapers/ | rofi -dmenu -no-config -theme ~/.config/rofi/wallpaper.rasi)
+
 for monitor in $monitors; do
 	# wallpaper=$(fd ".png|.jpg|.jpeg" ~/source/dotfiles/wallpapers/ | shuf -n1)
-	wallpaper=$(fd ".png|.jpg|.jpeg" ~/source/dotfiles/wallpapers/ | rofi -dmenu -no-config -theme ~/.config/rofi/wallpaper.rasi)
 	echo "preload = $wallpaper" >> ~/.config/hypr/hyprpaper.conf
 	echo "wallpaper = $monitor,$wallpaper" >> ~/.config/hypr/hyprpaper.conf
 done
