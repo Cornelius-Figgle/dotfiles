a88e4891539de3189de2010c5e9976f04775f4c6
Cornelius-Figgle <max@fullimage.net>
2023-08-05 10:36:24 +0000

    tmux belaugh

--- scripts/mc_start.sh
+++ scripts/mc_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tmux new -s MainMc -d
-tmux send-keys -t MainMc:0.0 "cd /home/max/mc/current" ENTER
-tmux send-keys -t MainMc:0.0 "/home/max/mc/current/bedrock_server"  ENTER
+tmux send-keys -t MainMc:1.1 "cd /home/max/mc/current" ENTER
+tmux send-keys -t MainMc:1.1 "/home/max/mc/current/bedrock_server"  ENTER
 # tmux a
