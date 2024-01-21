3f4aee89e46219c2aff960930dcdb86c0ad9aa3c
Cornelius-Figgle <max@fullimage.net>
2024-01-21 10:13:46 +0000

    tofi

--- config/tofi/scripts/remote.sh
+++ config/tofi/scripts/remote.sh
@@ -4,62 +4,41 @@ function spawn_term_win {
   riverctl spawn "foot $1"
 }
 
-function ssh {
-  op=$(echo "nightcrawler2      (max)
-pve1               (root)
-mimic5             (root)
-irc2               (root)
-polaris            (max)
-other*" | tofi)
+function shoose {
+  op=$(echo "
+max@nightcrawler2                 [ssh]
+root@pve2                         [ssh]
+root@mimic5                       [ssh]
+max@polaris                       [ssh]
+root@irc2                         [ssh]
+root@homepage                     [ssh]
+max@win11-1                       [rdp]
+paul.harrison@quicksilver         [rdp]" | tofi)
 
   case $op in
-    "nightcrawler2      (max)")
+    "max@nightcrawler2                 [ssh]")
       spawn_term_win "ssh -t max@192.168.0.8 'TERM=xterm && tmux attach'"
       ;;
-    "pve1               (root)")
+    "root@pve2                         [ssh]")
       spawn_term_win "ssh root@192.168.0.19"
       ;;
-    "mimic5             (root)")
+    "root@mimic5                       [ssh]")
       spawn_term_win "ssh -t root@192.168.0.12 'TERM=xterm && tmux attach'"
       ;;
-    "irc2               (root)")
-      spawn_term_win "ssh -t root@192.168.0.21 'TERM=xterm && tmux attach'"
-      ;;
-    "polaris            (max)")
+    "max@polaris                       [ssh]")
       spawn_term_win "ssh -t max@192.168.0.25 'TERM=xterm && tmux attach'"
       ;;
-  esac
-}
-
-function rdp {
-  op=$(echo "win11-1            (max)
-quicksilver        (paul.harrison)
-other*" | tofi)
-
-  case $op in
-    "win11-1            (max)")
+    "root@irc2                         [ssh]")
+      spawn_term_win "ssh -t root@192.168.0.21 'TERM=xterm && tmux attach'"
+      ;;
+    "root@homepage                     [ssh]")
+      spawn_term_win "ssh -t max@192.168.0.25 'TERM=xterm && tmux attach'"
+      ;;
+    "max@win11-1                       [rdp]")
       wlfreerdp -v 192.168.0.23 -u max --sec tls --disable-wallpaper --disable-themes --disable-fonts --disable-aero --disable-window-drag --disable-menu-anims -f
       ;;
-    "quicksilver        (paul.harrison)")
+    "paul.harrison@quicksilver         [rdp]")
       wlfreerdp -v 192.168.0.11 -d FULLIMAGE -u paul.harrison --sec tls --disable-wallpaper --disable-themes --disable-fonts --disable-aero --disable-window-drag --disable-menu-anims -f
-      ;;
-  esac
-}
-
-function shoose {
-  op=$(echo "ssh/
-rdp/
-vnc/" | tofi)
-
-  case $op in
-    "ssh/")
-      ssh
-      ;;
-    "rdp/")
-      rdp
-      ;;
-    "vnc/")
-    
       ;;
   esac
 }
