52d62086af5d5c2bc0fe551c278684953bb39177
Cornelius-Figgle <max@fullimage.net>
2024-02-23 20:23:33 +0000

    glaze

--- config/glaze-wm/scripts/autostart.bat
+++ config/glaze-wm/scripts/autostart.bat
@@ -4,7 +4,7 @@ START /B "" glazewm.exe
 
 :: START /B "" taskkill /IM explorer.exe /F
 
-START /B "" "%USERPROFILE%\.glaze-wm\scripts\ahk_exit.ahk"
-START /B "" "%USERPROFILE%\.glaze-wm\scripts\fn_macros.ahk"
-START /B "" "%USERPROFILE%\.glaze-wm\scripts\media_keys.ahk"
-if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "%USERPROFILE%\.glaze-wm\scripts\win_key.ahk"
+:: START /B "" "%USERPROFILE%\.glaze-wm\scripts\ahk_exit.ahk"
+:: START /B "" "%USERPROFILE%\.glaze-wm\scripts\fn_macros.ahk"
+:: START /B "" "%USERPROFILE%\.glaze-wm\scripts\media_keys.ahk"
+:: if %COMPUTERNAME%==NIGHTCRAWLER START /B "" "%USERPROFILE%\.glaze-wm\scripts\win_key.ahk"
