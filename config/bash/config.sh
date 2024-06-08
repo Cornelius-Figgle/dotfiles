fa5a25c85e73000a125f78a8a25263248adaba1c
Cornelius-Figgle <max@fullimage.net>
2024-06-08 17:10:08 +0100

    nvm :/

--- config/bash/config.sh
+++ config/bash/config.sh
@@ -81,3 +81,7 @@ fi
 if [ -e ~/.bash/autostart.sh ]; then
 	. ~/.bash/autostart.sh
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
