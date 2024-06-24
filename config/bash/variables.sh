e9076cb6beda96f2e235725e4e73a5af92b3f418
Cornelius-Figgle <max@fullimage.net>
2024-06-24 16:19:05 +0000

    deb

--- config/bash/variables.sh
+++ config/bash/variables.sh
@@ -7,7 +7,12 @@
 
 export PATH="$PATH:/home/max/.local/bin:/home/max/.cargo/bin:/usr/local/bin"
 export XDG_SCREENSHOTS_DIR="/home/max/Screenshots"
-export EDITOR="/usr/bin/hx"
+
+if [ -f "/etc/debian_version" ]; then
+	export EDITOR="/usr/bin/hx"
+else
+	export EDITOR="/usr/bin/nano"
+fi
 
 # https://bash-prompt-generator.org/
 PS1='[\[\e[93m\]\u\[\e[0m\]@\[\e[95m\]\h\[\e[0m\]:\[\e[94m\]\w\[\e[0m\]\[\e[92m\]$( if [[ ! -z "$(git branch --show-current 2>/dev/null)" ]]; then echo " #$(git branch --show-current 2>/dev/null)" ; fi )\[\e[0m\]] \[\e[91m\]\$\[\e[0m\] '
