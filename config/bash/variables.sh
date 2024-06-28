d52115948d057ec427d6012a6868abf53cf1d9c6
Cornelius-Figgle <max@fullimage.net>
2024-06-28 17:19:11 +0100

    yazi

--- config/bash/variables.sh
+++ config/bash/variables.sh
@@ -15,7 +15,8 @@ else
 fi
 
 # https://bash-prompt-generator.org/
-PS1='[\[\e[93m\]\u\[\e[0m\]@\[\e[95m\]\h\[\e[0m\]:\[\e[94m\]\w\[\e[0m\]\[\e[92m\]$( if [[ ! -z "$(git branch --show-current 2>/dev/null)" ]]; then echo " #$(git branch --show-current 2>/dev/null)" ; fi )\[\e[0m\]] \[\e[91m\]\$\[\e[0m\] '
+# https://github.com/Sonico98/yazi-prompt.sh/blob/master/posix/yazi.sh
+PS1='[\[\e[93m\]\u\[\e[0m\]@\[\e[95m\]\h\[\e[0m\]:\[\e[94m\]\w\[\e[0m\]\[\e[96m\]$(if [ -n "$YAZI_LEVEL" ]; then echo " (yazi)" ; fi)\[\e[92m\]$( if [[ ! -z "$(git branch --show-current 2>/dev/null)" ]]; then echo " #$(git branch --show-current 2>/dev/null)" ; fi )\[\e[0m\]] \[\e[91m\]\$\[\e[0m\] '
 
 export PYTHONIOENCODING="utf-8"
 # eval "$(thefuck --alias)"
