df361ffe7cb0171a8f0256aa9373a96faadb5a6b
Cornelius-Figgle <max@fullimage.net>
2024-06-28 16:22:23 +0100

    z

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -88,7 +88,7 @@ function yy() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
+		z "$cwd"
 	fi
 	rm -f -- "$tmp"
 }
