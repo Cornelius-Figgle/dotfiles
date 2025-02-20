0db0c97986a473659f8fde51df7bc6823af12328
Cornelius-Figgle <max@fullimage.net>
2025-02-20 17:14:50 +0000

    wezssh

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -1,61 +1,12 @@
 alias cls='clear'
 alias ff='fastfetch'
 
-function poseidon() {
-	ssh max@192.168.0.8 "$*"
-}
-function zeus() {
-	ssh root@192.168.0.19 "$*"
-}
-function mnemosyne() {
-	ssh root@192.168.0.26 "$*"
-}
-function dionysus() {
-	ssh max@192.168.0.12 "$*"
-}
-function iris() {
-	ssh root@192.168.0.21 "$*"
-}
-function athena() {
-	ssh max@192.168.0.25 "$*"
-}
-function hectia() {
-	ssh root@192.168.0.27 "$*"
-}
-function hephaestus() {
-	ssh max@192.168.0.28 "$*"
-}
-function clio() {
-	ssh root@192.168.0.30 "$*"
-}
-function hera() {
-	ssh max@192.168.0.31 "$*"
+function wezsshu() {
+	wezterm ssh max@192.168.0.$1
 }
 
-function mnt() {
-	case $1 in
-		"athena\auth")
-			sudo mount -t cifs -o credentials=/mnt/.athena,uid=1000,gid=1000 //192.168.0.25/auth /mnt/smb/athena/auth
-		;;
-		"athena\music")
-			sudo mount -t cifs -o credentials=/mnt/.athena,uid=1000,gid=1000 //192.168.0.25/music /mnt/smb/athena/music
-		;;
-		"athena\notes")
-			sudo mount -t cifs -o credentials=/mnt/.athena,uid=1000,gid=1000 //192.168.0.25/notes /mnt/smb/athena/notes
-		;;
-		"athena\setup")
-			sudo mount -t cifs -o credentials=/mnt/.athena,uid=1000,gid=1000 //192.168.0.25/setup /mnt/smb/athena/setup
-		;;
-		"svr1\Media")
-			sudo mount -t cifs -o credentials=/mnt/.fullimage,file_mode=0755,dir_mode=0755 //192.168.0.10/Media /mnt/smb/svr1/Media
-		;;
-		"dc1\Public")
-			sudo mount -t cifs -o credentials=/mnt/.fullimage,file_mode=0755,dir_mode=0755 //192.168.0.15/Public /mnt/smb/dc1/Public
-		;;
-		"dc1\Techincal")
-			sudo mount -t cifs -o credentials=/mnt/.fullimage,file_mode=0755,dir_mode=0755 //192.168.0.15/Technical /mnt/smb/dc1/Technical
-		;;
-	esac
+function wezsshr() {
+	wezterm ssh root@192.168.0.$1
 }
 
 alias dot='./bin/dotter deploy --force --verbose'
--- config/pwsh/config.ps1
+++ config/pwsh/config.ps1
@@ -9,13 +9,8 @@ New-Alias -Name ahk -Value autohotkey
 Function glances-alias-fn { glances --disable-irix --separator --programs @args }
 # New-Alias -Name glances -Value glances-alias-fn  # disabled bc pwsh is bloody slow
 
-Function poseidon { ssh max@192.168.0.8 }
-Function zeus { ssh root@192.168.0.19 }
-Function mnemosyne { ssh root@192.168.0.26 }
-Function dionysus { ssh max@192.168.0.12 }
-Function iris { ssh root@192.168.0.21 }
-Function athena { ssh max@192.168.0.25 }
-Function hephaestus { ssh max@192.168.0.28 }
+Function wezsshu { wezterm ssh max@192.168.0.$args[0] }
+Function wezsshr { wezterm ssh root@192.168.0.$args[0] }
 
 # ====================
 # Functions
@@ -24,7 +19,7 @@ Function hephaestus { ssh max@192.168.0.28 }
 Function svpush {
 	(git add .)
 	if ($?) {
-		(git commit -m $args[0])
+		(git commit -m @args)
 		if ($?) {
 			(git push)
 		}
