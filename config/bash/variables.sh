e3a6734b73d6246cf185ca0a9579b733700a8c58
Cornelius-Figgle <max@fullimage.net>
2025-07-20 10:40:04 +0100

    hx

--- config/bash/variables.sh
+++ config/bash/variables.sh
@@ -12,11 +12,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-if [ -f "/etc/debian_version" ]; then
-	export EDITOR="/usr/bin/nano"
-else
-	export EDITOR="/usr/bin/hx"
-fi
+export EDITOR="/usr/bin/hx"
+
 
 # https://bash-prompt-generator.org/
 # https://github.com/Sonico98/yazi-prompt.sh/blob/master/posix/yazi.sh
