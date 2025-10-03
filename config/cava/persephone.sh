b6b702500778e21c2a9521af6cd590ba9a5c42e7
Cornelius-Figgle <max@fullimage.net>
2025-10-03 10:02:05 +0100

    persephone

--- config/cava/persephone.sh
+++ config/cava/persephone.sh
@@ -6,11 +6,14 @@ mkfifo /tmp/cava.fifo
 echo "starting Winscap"
 /mnt/s/windows/Winscap/winscap.exe 2 48000 16 > /tmp/cava.fifo &
 
-echo "cava left"
-tmux send-keys "cava -p ~/.config/cava/left" ENTER
+echo "cava averaged"
+tmux send-keys "cava -p ~/.config/cava/config" ENTER
 
-echo "split pane"
-tmux split-pane -v
+# echo "cava left"
+# tmux send-keys "cava -p ~/.config/cava/left" ENTER
 
-echo "cava right"
-tmux send-keys "cava -p ~/.config/cava/right" ENTER
+# echo "split pane"
+# tmux split-pane -v
+
+# echo "cava right"
+# tmux send-keys "cava -p ~/.config/cava/right" ENTER
