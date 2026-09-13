3774afde4fd5d2dd9c8a17e17bbce3c74b34bc04
Cornelius-Figgle <max@fullimage.net>
2026-09-13 21:55:40 +0100

    sshl

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -1,21 +1,34 @@
 alias cls='clear'
 alias ff='fastfetch'
+alias dot='./bin/dotter deploy --force --verbose'
+alias wl='dbus-run-session river'
+alias qh="echo $(whoami) @ $(hostname) on $(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p') for $(uptime | cut -d' ' -f5 | cut -d',' -f1)"
 
-function wezsshu() {
-	nohup wezterm ssh max@10.0.10.$1 &
+if [[ "$(hostname)" == "poseidon" ]]; then
+	chooser="tofi"
+else
+	chooser="fzf"
+fi
+
+function spawn_ssh_win {
+	if [[ "$(hostname)" == "poseidon" ]]; then
+		riverctl spawn "wezterm ssh $1"
+	else
+		ssh $1
+	fi
 }
 
-function wezsshr() {
-	nohup wezterm ssh root@10.0.10.0.$1 &
+function spawn_rdp {
+	if [[ "$(hostname)" == "poseidon" ]]; then
+		riverctl spawn "xfreerdp -v $2 -u $1 --sec tls --disable-wallpaper --disable-themes --disable-fonts --disable-aero --disable-window-drag --disable-menu-anims -f"
+	else
+		xfreerdp -v $2 -u $1 --sec tls --disable-wallpaper --disable-themes --disable-fonts --disable-aero --disable-window-drag --disable-menu-anims -f
+	fi
 }
 
 function jqbat() {
 	jq . $1 | bat --language json
 }
-
-alias dot='./bin/dotter deploy --force --verbose'
-
-alias wl='dbus-run-session river'
 
 function svpush() {
 	git add .
@@ -29,7 +42,7 @@ function cddir() {
 }
 
 function visl() {
-	if [[ $(hostname) -eq "persephone" ]]; then
+	if [[ "$(hostname)" == "persephone" ]]; then
 		# winscap
 		mkfifo /tmp/cava.fifo
 		/mnt/p/01\ Homelab/01\ Software/Winscap/winscap.exe 2 44100 16 > /tmp/cava.fifo &
@@ -46,22 +59,55 @@ function visl() {
 	fi
 }
 
+function sshl() {
+  # declare arrays
+  hostnames=()
+  commands=()
+
+	# use local directory if possible
+	if [ -d "/home/max/syncthing" ]; then
+		machines_dir="/home/max/syncthing/00 Documentation/Machines/"
+	else
+		machines_dir="/mnt/smb/athena/public/01 Homelab/00 Documentation/Machines/"
+	fi
+
+  # loop over files
+  for path in "$machines_dir"*.md; do
+    # get data
+    name=$(basename "$path" .md)
+    user=$(grep "Main user:" "$path" | cut -d':' -f 2 | cut -d' ' -f 2 | cut -d'"' -f 2)
+    ip=$(grep "IP Ext:" "$path" | cut -d':' -f 2 | cut -d' ' -f 2 | cut -d'"' -f 2)
+    os=$(grep "OS:" "$path" | cut -d':' -f 2 | cut -d' ' -f 2 | cut -d'"' -f 2)
+
+    if [ ! -z "$user" ] && [ ! -z "$ip" ] && [ "$ip" != "DCHP" ]; then
+      # populate arrays
+      hostnames+=( "$name" )
+
+      if [ "$os" == "Windows" ]; then
+        commands+=( "rdp $user 10.0.10.$ip" )
+      else
+        commands+=( "ssh $user@10.0.10.$ip" )
+      fi
+    fi
+  done
+
+  # get host
+  op=$(for i in ${!hostnames[@]}; do echo "${hostnames[$i]}"; done | $chooser)
+
+  # run command
+  for i in ${!hostnames[@]}; do
+    if [ "$op" == "${hostnames[$i]}" ]; then
+      if [ "$(echo ${commands[$i]} | cut -d' ' -f 1)" == "rdp" ]; then
+        spawn_rdp "$(echo ${commands[$i]} | cut -d' ' -f 2)" "$(echo ${commands[$i]} | cut -d' ' -f 3)"
+      else
+        spawn_ssh_win "$(echo ${commands[$i]} | cut -d' ' -f 2)"
+      fi
+    fi
+  done
+}
+
 function 0x0() {
 	$* > /tmp/bash_0x0.txt
 	curl -F'file=@/tmp/bash_0x0.txt' https://0x0.st/
 	rm /tmp/bash_0x0.txt
-}
-
-function qh() {
-		echo $(whoami) @ $(hostname) on $(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p') for $(uptime | cut -d' ' -f5 | cut -d',' -f1)
-}
-
-# https://yazi-rs.github.io/docs/quick-start#shell-wrapper
-function yy() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd "$cwd"  # https://github.com/sxyazi/yazi/issues/978
-	fi
-	rm -f -- "$tmp"
 }
